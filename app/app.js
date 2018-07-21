dcf4b50a40924e68bb0315d30986af4d319f9e6c
Stephen Andrews <stephen@andrewstribe.co.nz>
2018-07-22 11:33:33 +1200

    Small fixes

--- app/app.js
+++ app/app.js
@@ -7,49 +7,37 @@ var app = angular.module('popup', [
 
 app.config(function($routeProvider) {
     $routeProvider
-    .when("/create", {
-        templateUrl : "app/views/create.html",
-        controller : "CreateController",
-    })
-    .when("/unlock", {
-        templateUrl : "app/views/unlock.html",
-        controller : "UnlockController",
-    })
     .when("/new", {
         templateUrl : "app/views/new.html",
         controller : "NewController",
+    })
+    .when("/create", {
+        templateUrl : "app/views/create.html",
+        controller : "CreateController",
     })
     .when("/restore", {
         templateUrl : "app/views/restore.html",
         controller : "RestoreController",
     })
-    .when("/main", {
-        templateUrl : "app/views/main.html",
-        controller : "MainController",
-    })
-    .when("/send", {
-        templateUrl : "app/views/send.html",
-        controller : "SendController",
-    })
-    .when("/setting", {
-        templateUrl : "app/views/setting.html",
-        controller : "SettingController",
-    })
-    .when("/delegate", {
-        templateUrl : "app/views/delegate.html",
-        controller : "DelegateController",
-    })
-    .when("/qr", {
-        templateUrl : "app/views/qr.html",
-        controller : "QrController",
+    .when("/validate", {
+        templateUrl : "app/views/validate.html",
+        controller : "ValidateController",
     })
     .when("/encrypt", {
         templateUrl : "app/views/encrypt.html",
         controller : "EncryptController",
     })
-    .when("/validate", {
-        templateUrl : "app/views/validate.html",
-        controller : "ValidateController",
+    .when("/main", {
+        templateUrl : "app/views/main.html",
+        controller : "MainController",
+    })
+    .when("/unlock", {
+        templateUrl : "app/views/unlock.html",
+        controller : "UnlockController",
+    })
+    .when("/setting", {
+        templateUrl : "app/views/setting.html",
+        controller : "SettingController",
     })
     .otherwise({
         redirectTo: '/new'

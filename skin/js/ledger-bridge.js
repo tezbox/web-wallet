8d60d96f952eee1139f1a62d545f4c7c06aa5668
Stephen Andrews <stephen@andrewstribe.co.nz>
2018-10-16 01:03:03 +1300

    Updating the ledger bridge

--- skin/js/ledger-bridge.js
+++ skin/js/ledger-bridge.js
@@ -2,17 +2,22 @@ document.addEventListener("message", function(e){
   if (e && e.data && e.data.target === 'LEDGER-IFRAME') {
     switch (e.data.action) {
       case 'getAddress':
-          window.tezledger.getAddress(e.data.path).then(function(r){sendMessageToExtension(e.data.action, r)});
+          window.tezledger.getAddress(e.data.path)
+          .then(function(r){sendMessageToExtension(e.data.action, true, r)})
+          .catch(function(r){sendMessageToExtension(e.data.action, false, r)});
       break;
       case 'sign':
-          window.tezledger.sign(e.data.path, e.data.bytes).then(function(r){sendMessageToExtension(e.data.action, r)});
+          window.tezledger.sign(e.data.path, e.data.bytes)
+          .then(function(r){sendMessageToExtension(e.data.action, true, r)})
+          .catch(function(r){sendMessageToExtension(e.data.action, false, r)});
       break;
     }
   }
 }, false);
-function sendMessageToExtension(action, data) {
+function sendMessageToExtension(action, success, data) {
   window.parent.postMessage({
     action : action,
+    success : success,
     data : data
   }, '*')
 }
